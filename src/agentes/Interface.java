171e90e685414d90f9d18572a223469613e13081
Wilson Daniel <wdospinal@unal.edu.co>
2016-05-05 11:50:00 -0500

    Refactor interface.java

--- src/agentes/Interface.java
+++ src/agentes/Interface.java
@@ -5,44 +5,35 @@ import gui.principal;
 import jade.util.leap.List;
 import jade.core.*;
 import jade.core.behaviours.*;
-import jade.lang.acl.*;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPANames;
-import jade.domain.DFService;
-import jade.domain.FIPAException;
- 
+import jade.lang.acl.*; 
 import jade.content.*;
 import jade.content.lang.*;
+import jade.content.lang.Codec.CodecException;
 import jade.content.lang.sl.*;
 import jade.content.onto.*;
 import jade.util.leap.ArrayList;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import javax.swing.JFrame;
  
 public class Interface extends Agent {
  
-    private Codec codec = new SLCodec();
-    private Ontology ontologia = CalendarOntology.getInstance();
+private final Codec CODEC = new SLCodec();
+private final Ontology ONTOLOGIA = CalendarOntology.getInstance();    
+private JFrame miventana;   
     
-  private JFrame miventana;
-    // Clase que describe el comportamiento que permite recibir un mensaje
-    // y contestarlo
-    
-    
-        class EnviarMensajeBehaviour extends SimpleBehaviour {
+class EnviarMensajeBehaviour extends SimpleBehaviour {
  
-      private boolean finished = false;
+    private boolean terminado = false;
  
     public EnviarMensajeBehaviour(Agent a) {
         super(a);
     }
  
     public void action() {
-        try
-    {
-        System.out.println("\nRealizado por: \nAldo Esteban Garces Correa \nAndres Felipe Gonzalez Bermudez \nWilson Daniel Ospina Leon");
+        try{
+            System.out.println("\nRealizado por: \nAldo Esteban Garces Correa \nAndres Felipe Gonzalez Bermudez \nWilson Daniel Ospina Leon");
             System.out.println("\nIntroduce el nombre del agente interface (el nombre dado al Agente al lanzar el -container): ");
             BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
             String respuesta = buff.readLine();
@@ -51,17 +42,19 @@ public class Interface extends Agent {
             ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
             msg.setSender(getAID());
             msg.addReceiver(r);
-            msg.setLanguage(codec.getName());
-               msg.setOntology(ontologia.getName());
- 
+            msg.setLanguage(CODEC.getName());
+            msg.setOntology(ONTOLOGIA.getName());
+
             System.out.println("\nIntroduce el NOMBRE del grupo:");
             respuesta = buff.readLine();
             Grupo g = new Grupo();
             g.setNombre(respuesta);
+
             System.out.println("\nIntroduce el NOMBRE del 1er estudiante:");
             respuesta = buff.readLine();
             Estudiante e1 = new Estudiante();
             e1.setNombre(respuesta);
+
             System.out.println("\nIntroduce el NOMBRE del 2er estudiante:");
             respuesta = buff.readLine();
             Estudiante e2 = new Estudiante();
@@ -73,112 +66,95 @@ public class Interface extends Agent {
             BD bd = new BD();
             bd.setGrupos(g);
             getContentManager().fillContent(msg, bd);
- 
+
             send(msg);
-           }
-           catch (java.io.IOException io)
-            {System.out.println(io);
         }
-        catch (jade.content.lang.Codec.CodecException ce) {
-               System.out.println(ce);
+        catch (IOException | jade.content.lang.Codec.CodecException | jade.content.onto.OntologyException io){
+           System.out.println(io);
         }
-        catch (jade.content.onto.OntologyException oe) {
-            System.out.println(oe);
+        catch (Exception e){
+            System.out.println("\n\n... Terminando ...");
+            terminado = true;
         }
-    catch (Exception e){
-        System.out.println("\n\n... Terminando ...");
-        finished=true;
-    }
     }
  
-    public boolean done() {
- 
-     return true;
- 
+    public boolean done() { 
+     return true; 
     }
-    }
-    
-    
-    
-    class WaitPingAndReplyBehaviour extends SimpleBehaviour {
-      private boolean finished = false;
+}
+     
+  class WaitPingAndReplyBehaviour extends SimpleBehaviour {
+    private boolean TERMINADO = false;
  
-      public WaitPingAndReplyBehaviour(Agent a) {
+    public WaitPingAndReplyBehaviour(Agent a) {
         super(a);
-      }
+    }
+    public void action() {
+        
+        System.out.println("\nEsperando estudiantes");
  
-      public void action() {
- 
-          
-    System.out.println("\nEsperando estudiantes");
- 
-    MessageTemplate mt = MessageTemplate.and(
-            MessageTemplate.MatchLanguage(codec.getName()),
-            MessageTemplate.MatchOntology(ontologia.getName()));
+        MessageTemplate mt = MessageTemplate.and(
+                MessageTemplate.MatchLanguage(CODEC.getName()),
+                MessageTemplate.MatchOntology(ONTOLOGIA.getName()));
         ACLMessage  msg = blockingReceive(mt);
  
-        try {
- 
-        if(msg != null){
-        if(msg.getPerformative() == ACLMessage.NOT_UNDERSTOOD){
-                System.out.println("Mensaje NOT UNDERSTOOD recibido");
-            }
-        else{
-            if(msg.getPerformative()== ACLMessage.INFORM){
- 
-            ContentElement ce = getContentManager().extractContent(msg);
-            if (ce instanceof BD){
-                // Recibido un INFORM con contenido correcto
-                BD bd = (BD) ce;
-                
-                Grupo of = bd.getGrupos();
-                System.out.println("Mensaje recibido:");
-                System.out.println("Nombre del grupo: " + of.getNombre());
-                List integrantes= of.getEstudiantes();
-                for(int i=0;i<integrantes.size();i++){
-                 Estudiante e = (Estudiante) integrantes.get(i);
-                System.out.println("Nombre estudiante: " + e.getNombre());
-                }
+        try { 
+            if(msg != null){
+                if(msg.getPerformative() == ACLMessage.NOT_UNDERSTOOD){
+                    System.out.println("Mensaje NOT UNDERSTOOD recibido");
                 }
             else{
-                // Recibido un INFORM con contenido incorrecto
-                ACLMessage reply = msg.createReply();
-                reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
-                reply.setContent("( UnexpectedContent (expected ping))");
-                send(reply);
+                if(msg.getPerformative()== ACLMessage.INFORM){
+                ContentElement ce = getContentManager().extractContent(msg);
+                    if (ce instanceof BD){
+                        BD bd = (BD) ce;
+
+                        Grupo of = bd.getGrupos();
+                        System.out.println("Mensaje recibido:");
+                        System.out.println("Nombre del grupo: " + of.getNombre());
+                        List integrantes= of.getEstudiantes();
+                        for(int i=0;i<integrantes.size();i++){
+                         Estudiante e = (Estudiante) integrantes.get(i);
+                        System.out.println("Nombre estudiante: " + e.getNombre());
+                        }
+                }
+                else{
+                    // Recibido un INFORM con contenido incorrecto
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+                    reply.setContent("( UnexpectedContent (expected ping))");
+                    send(reply);
+                }
+                }
+                else {
+                    // Recibida una performativa incorrecta
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+                    reply.setContent("( (Unexpected-act "+ACLMessage.getPerformative(msg.getPerformative())+")( expected (inform)))");
+                    send(reply);
+                }
             }
+            }else{
+            //System.out.println("No message received");
             }
-            else {
-                // Recibida una performativa incorrecta
-                ACLMessage reply = msg.createReply();
-                reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
-                reply.setContent("( (Unexpected-act "+ACLMessage.getPerformative(msg.getPerformative())+")( expected (inform)))");
-                send(reply);
-            }
-        }
-        }else{
-        //System.out.println("No message received");
-        }
- 
+
          }
-         catch (jade.content.lang.Codec.CodecException ce) {
+         catch (CodecException | OntologyException ce) {
                System.out.println(ce);
-        }
-        catch (jade.content.onto.OntologyException oe) {
-            System.out.println(oe);
         }
      }
  
       public boolean done() {
-        return finished;
+        return TERMINADO;
       }
  
-  } //Fin de la clase WaitPingAndReplyBehaviour
+  } 
  
+  @Override
   protected void setup() {
  
-    getContentManager().registerLanguage(codec);
-    getContentManager().registerOntology(ontologia);
+    getContentManager().registerLanguage(CODEC);
+    getContentManager().registerOntology(ONTOLOGIA);
     miventana=new principal(this);
     miventana.setVisible(true);
 
